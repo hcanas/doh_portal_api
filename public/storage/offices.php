87893eaff81e3d306a580c0c3399dccc64a143d2
hcanas <harveyanas@gmail.com>
2022-10-03 09:48:00 +0800

    upload ID files

---
+++ public/storage/offices.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.0
+ */
+
+/**
+ * Database `portal_dev`
+ */
+
+/* `portal_dev`.`offices` */
+return array(
+  array('id' => '1','name' => 'Office of the Regional and the Assistant Regional Director','short_name' => 'RD-ARD','parent_id' => NULL,'created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '2','name' => 'Health Facility Development and Enhancement Unit','short_name' => 'HFDEU','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '3','name' => 'Health Emergency Management Section','short_name' => 'HEMS','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '4','name' => 'Planning Section','short_name' => 'Planning','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '5','name' => 'Legal Section','short_name' => 'Legal','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '6','name' => 'Health Promotion and Communications Management Unit','short_name' => 'HPCMU','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '7','name' => 'Local Health System Section','short_name' => 'LHSS','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '8','name' => 'Baguio General Hospital and Medical Center','short_name' => 'BGHMC','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '9','name' => 'Luis Hora Regional Memorial Hospital','short_name' => 'LHRMH','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '10','name' => 'Far North Luzon General Hospital and Training Center','short_name' => 'FNLGHTC','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '11','name' => 'Conner District Hospital','short_name' => 'CDH','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '12','name' => 'City Department of Health Office - Baguio City','short_name' => 'CDOHO-Baguio City','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '13','name' => 'Provincial Department of Health Office - Abra','short_name' => 'PDOHO-Abra','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '14','name' => 'Provincial Department of Health Office - Apayao','short_name' => 'PDOHO-Apayao','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '15','name' => 'Provincial Department of Health Office - Benguet','short_name' => 'PDOHO-Benguet','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '16','name' => 'Provincial Department of Health Office - Ifugao','short_name' => 'PDOHO-Ifugao','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '17','name' => 'Provincial Department of Health Office - Kalinga','short_name' => 'PDOHO-Kalinga','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '18','name' => 'Provincial Department of Health Office - Mt. Province','short_name' => 'PDOHO-Mt. Province','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '19','name' => 'Management Support Division','short_name' => 'MSD','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '20','name' => 'Bids and Awards Committee','short_name' => 'BAC','parent_id' => '19','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '21','name' => 'Procurement Team','short_name' => 'PT','parent_id' => '19','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '22','name' => 'Accounting Section','short_name' => 'Accounting','parent_id' => '19','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '23','name' => 'Cashiering Section','short_name' => 'Cashiering','parent_id' => '19','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '24','name' => 'Records Section','short_name' => 'Records','parent_id' => '19','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '25','name' => 'Budget Section','short_name' => 'Budget','parent_id' => '19','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '26','name' => 'General Services, Transport, Dormitory, and Training Center','short_name' => 'GSO','parent_id' => '19','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '27','name' => 'Information and Communications Technology','short_name' => 'ICT','parent_id' => '19','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '28','name' => 'Logistics Management Section','short_name' => 'LMS','parent_id' => '19','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '29','name' => 'Human Resource Management and Development Section - Training','short_name' => 'HR-Training','parent_id' => '19','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '30','name' => 'Human Resource Management and Development Section - Personnel','short_name' => 'HR-Personnel','parent_id' => '19','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '31','name' => 'Local Health Support Division','short_name' => 'LHSD','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '32','name' => 'Non-communicable Diseases Cluster','short_name' => 'NCD','parent_id' => '31','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '33','name' => 'Communicable Diseases Cluster','short_name' => 'CDC','parent_id' => '31','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '34','name' => 'Family Health Cluster','short_name' => 'FHC','parent_id' => '31','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '35','name' => 'Research, Epidemiology, Surveillance, and Statistics Cluster','short_name' => 'RESSU','parent_id' => '31','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '36','name' => 'Regulation, Licensing, and Enforcement Division','short_name' => 'RLED','parent_id' => '1','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '37','name' => 'Licensing Section','short_name' => 'Licensing','parent_id' => '36','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '38','name' => 'Accreditation Section','short_name' => 'Accreditation','parent_id' => '36','created_at' => '2022-09-14 18:38:41','updated_at' => '2022-09-14 18:38:41'),
+  array('id' => '40','name' => 'Placeholder','short_name' => 'NA','parent_id' => '1','created_at' => '2022-09-22 00:10:44','updated_at' => '2022-09-22 00:35:14')
+);
